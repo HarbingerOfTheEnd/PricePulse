058e3d56a088e06086bb90fcaea5f54a5a1a4bd8
Sai Kumar <ksai44576@gmail.com>
2025-05-21 12:54:44 +0530

    Change amazon_url field type in TrackedProduct model to HttpUrl for improved validation

--- backend/app/models/tracked_product.py
+++ backend/app/models/tracked_product.py
@@ -1,3 +1,4 @@
+from pydantic import HttpUrl
 from sqlmodel import Field, Relationship, SQLModel
 
 from backend.app.models.user import User
@@ -6,7 +7,7 @@ from backend.app.models.user import User
 class TrackedProduct(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
-    amazon_url: str = Field(index=True)
+    amazon_url: HttpUrl = Field(index=True)
 
     issued_by_id: int | None = Field(default=None, foreign_key="user.id")
     issued_by: User | None = Relationship(back_populates="tracked_products")
